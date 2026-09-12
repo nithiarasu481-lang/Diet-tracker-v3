ab448e21ed82e1fff52f60a347682fab7852683d
Nithiarasu.N <nithiarasu481@gmail.com>
2026-09-12 11:40:44 +0530

    Add MainActivity to load local HTML in WebView

---
+++ app/src/main/java/com/diettracker/v3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.diettracker.v3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView web = new WebView(this);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        web.loadUrl("file:///android_asset/index.html");
+
+        setContentView(web);
+    }
+}
